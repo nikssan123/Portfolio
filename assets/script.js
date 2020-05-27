f3c084c49169c9b60c9eadbcc95ed5a38b359607
nikssan123 <nikssan123@gmail.com>
2020-05-28 02:44:57 +0300

    make project section, add dummy sections

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+$(window).on("scroll", () => {
+    
+    let yOffset = window.pageYOffset;
+    if(yOffset > 80){
+        $(".scroll").css("display", "none");
+        
+    }
+    
+    if(yOffset > 100){
+        $("nav").fadeOut(500 , () => {
+        });
+        $("header h2").addClass("animate");
+    }
+    
+
+
+    if(yOffset === 0){
+        $(".scroll").css("display", "block");
+        $("nav").fadeIn("slow", () => {
+        });
+        $("header h2").removeClass("animate");
+    }
+});
+
+$(".scroll").on("click", () => {
+    $(".about").get(0).scrollIntoView();
+});
+
+
+function about(){
+    $(".about").get(0).scrollIntoView();
+}
+
+function projects(){
+    $(".projects").get(0).scrollIntoView();
+}
+
+function contact(){
+    $(".contact").get(0).scrollIntoView();
+}
+
+$(".buttons .code").on("click", () => {
+    alert("Show code");
+});
+
+$(".buttons .demo").on("click", () => {
+    alert("Show demo");
+});
